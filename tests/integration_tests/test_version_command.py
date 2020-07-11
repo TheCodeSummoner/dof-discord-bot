e8b6da4e8d09935823d3a586bcff68384795b536
Kacper <k.florianski@newcastle.ac.uk>
2020-07-11 10:49:47 +0200

    Added basic structure of the test, to be filled by Tom

--- tests/integration_tests/test_version_command.py
+++ tests/integration_tests/test_version_command.py
@@ -1,6 +1,7 @@
 """
 Tests associated with the !version command.
 """
+import discord
 import pytest
 import dof_discord_bot
 from . import helpers
@@ -32,3 +33,39 @@ async def test_returns_current_version():
 
     await helpers.get_test_channel().send("!version")
     await helpers.wait_for(version_to_appear, or_fail)
+
+
+@helpers.threaded_async
+async def test_returns_error_on_missing_role():
+    """
+    TODO: Tom to add documentation
+
+    TODO: Tests' README.md should mention that before running the code you should make sure dof tester bot has the defender role
+    """
+    async def error_to_appear():
+        """
+        Calling the command without a Defender role should result in the error message being printed.
+        """
+        # TODO: Write code ;)
+        pass
+
+    def or_fail():
+        """
+        Otherwise, fail the test.
+        """
+        pytest.fail("Timed out waiting for response to the !version command")
+
+    def get_tester_bot_member() -> discord.Member:
+        """
+        Helper command to retrieve the testing bot using the unique identifier.
+        """
+        for member in helpers.dof_bot.get_all_members():
+            if member.id == 720954603046305855:
+                return member
+
+    tester_bot_member = get_tester_bot_member()
+    defender_role = discord.utils.get(helpers.dof_bot.guild.roles, name="test")  # TODO: Change this to use the "Defneder" role in the final code
+    await tester_bot_member.remove_roles(defender_role)
+    await helpers.get_test_channel().send("!version")
+    await helpers.wait_for(error_to_appear, or_fail)
+    await tester_bot_member.add_roles(defender_role)
